4aa078ad411083206967fcb3f76d1b0ece8e03c1
Chris <chris.kuiper@gmail.com>
2016-04-08 09:11:38 -0700

    add feature where if no answers, an exclamation glyphicon appears

---
+++ app/helpers/question-popularity.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export function questionPopularity(params) {
+  var question = params[0];
+
+  if(question.get('answers').get('length') < 1) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-exclamation-sign"></span>');
+  }
+  else {
+    if(question.get('answers').get('length') >= 1) {
+      return Ember.String.htmlSafe('<span class="glyphicon glyphicon-ok"></span>');
+    }
+  }
+}
+
+export default Ember.Helper.helper(questionPopularity);
