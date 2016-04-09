feb6f61536cd6446aa0c098b03d374c8c49380bf
Chris Kuiper <chris.kuiper@gmail.com>
2016-04-09 09:53:15 -0700

    add lat and long to json file

--- app/components/google-maps.js
+++ app/components/google-maps.js
@@ -8,7 +8,7 @@ export default Ember.Component.extend({
         var container = this.$('.map-display')[0];
         var options = {
           center: this.get('map').center(question.get('latitude'), question.get('longitude')),
-          zoom: 15
+          zoom: 12
         };
         this.get('map').findMap(container, options);
       }
